9ded66988556d69851fd36d4d62446c52e16957a
Akashakhan <akashakhan11@gmail.com>
2025-10-24 21:07:38 +0530

    env set for creds

--- src/app/api/send-email/route.ts
+++ src/app/api/send-email/route.ts
@@ -1,7 +1,7 @@
 import { Resend } from 'resend';
 import { NextRequest, NextResponse } from 'next/server';
 
-const resend = new Resend('re_Y7TUpmCf_9DHjTX9UD2i6z9PMyuGL6qDp');
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 export async function POST(request: NextRequest) {
   try {
